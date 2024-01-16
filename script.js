c16c4269a20fd3cf7861f0f338c87fcf5fad8bd4
takumi shinzato <takumusic0873@gmail.com>
2024-01-16 23:38:28 +0900

    変数名を修正

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let $blank = document.querySelectorAll(".blank");
-for (let i = 0; i < $blank.length; i++) {
-    $blank[i].addEventListener("click", () => {
-        $blank[i].classList.toggle("true");
+let blank = document.querySelectorAll(".blank");
+for (let i = 0; i < blank.length; i++) {
+    blank[i].addEventListener("click", () => {
+        blank[i].classList.toggle("true");
     })
 }
